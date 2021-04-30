0aa1c1d755d0f8abec6d310329edf50840a340c2
kike <arellanestorillo@yahoo.com>
2021-04-29 23:06:51 -0500

    se agregan clases del curso de fundamentos

---
+++ FundamentosJavascript/clase1.js
@@ -0,0 +1,11 @@
+var nombre = 'Enrique' , apellido = 'arellanes';
+
+console.log('Hola ' + nombre + ' ' + apellido );
+
+//javascript en un lenguaje debilmente tipado 
+//por lo cual las variables pueden cambiar de tipo
+//ejemplo
+
+var edad = 25; //comienza como numero
+
+edad = '25años' //puede volverse un string

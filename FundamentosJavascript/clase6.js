0e2730519b63e61303c14128e241b374806cda41
kike <arellanestorillo@yahoo.com>
2021-05-06 22:09:23 -0500

    clase 6 de objetos

---
+++ FundamentosJavascript/clase6.js
@@ -0,0 +1,31 @@
+var Enrique = {
+    nombre: 'Enrique',
+    apellido: 'Arellanes',
+    edad: 25
+}
+
+//primer forma de desglosar un objeto
+function imprimirNombre1 (nombre) {
+    nombre = nombre.toUpperCase();
+    console.log(nombre);
+}
+
+imprimirNombre1(Enrique.nombre);
+
+//segunda forma de desglosar un objeto
+function imprimirNombre2 (persona) {
+   var nombre = persona.nombre.toUpperCase();
+    console.log(nombre);
+}
+
+imprimirNombre2(Enrique);
+
+
+//tercer forma de desglosar un objeto
+
+function imprimirNombre ({nombre}) {
+console.log(nombre);
+}
+
+imprimirNombre(Enrique);
+
